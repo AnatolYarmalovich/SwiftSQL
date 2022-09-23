23c2917b6518e33e269013599d5435ee5fbe0b5b
AnatolYarmolovich <yarmolovich1986@gmail.com>
2022-09-23 13:03:23 +0200

    initial commit

---
+++ SwiftSQL/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftSQL
+//
+//  Created by Anatol Uarmolovich on 23.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftSQL/SwiftSQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSQLApp.swift
+//  SwiftSQL
+//
+//  Created by Anatol Uarmolovich on 23.09.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
